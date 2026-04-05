92ac2e65ad64f5b556317e3fdddaca7ff8468afe
Er-qeanut <381995807@qq.com>
2026-04-05 23:53:28 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,55 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano",
+      "name": "pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8161904121522632,
+        "pitch": -0.007631993150880589,
+        "fov": 1.3633411682890908
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.8161904121522632,
+          "pitch": -0.007631993150880589,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.8161904121522632,
+          "pitch": -0.007631993150880589,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
